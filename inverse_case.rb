ffac4d35db930a5c0134f32e2404a2bc296f6f8b
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-27 14:46:14 +0530

    s

--- inverse_case.rb
+++
@@ -1,9 +0,0 @@
-class String
-	def to_s
-		self.swapcase
-	end
-end
-
-puts "Enter the string"
-input_string = gets.chomp
-puts input_string.to_s
--- regex.rb
+++
@@ -1,8 +0,0 @@
-def replacer(string)
-	regex = /[aeiou]/
-	puts string.downcase.gsub(regex,"*")
-end
-
-puts "Enter a String"
-input_string = gets.chomp
-replacer(input_string)
