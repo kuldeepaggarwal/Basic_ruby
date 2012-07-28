69a3f8ac66f4657c6d0135e1d8907b643a09d9f5
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-28 16:32:39 +0530

    s

---
+++ Highlight/lib/highlight.rb
@@ -0,0 +1,14 @@
+def highlight(original_string, highlighting_string)
+  @total_occurence = 0
+  temp = original_string.gsub(/(#{highlighting_string})/i) do |match|
+    @total_occurence = @total_occurence + 1 
+    "#{match.replace("(#{match})")}"
+  end
+  puts temp
+  puts "Total occurences: #{@total_occurence}"
+end
+puts "Enter the String"
+input_string = gets.chomp
+puts "Enter the highlighting string"
+highlight_string = gets.chomp
+highlight(input_string, highlight_string)
