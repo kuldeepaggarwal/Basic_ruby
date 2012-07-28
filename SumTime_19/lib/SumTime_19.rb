d483f5c1f67452afe03a07e8f4b46375689794f8
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-29 00:09:57 +0530

    s

--- SumTime_19/lib/SumTime_19.rb
+++ SumTime_19/lib/SumTime_19.rb
@@ -4,7 +4,7 @@ def sum(time1 = "00:00:00", time2 = "00:00:00")
   time_pattern = /^([2][0-3]|[0-1][0-9]):[0-5][0-9]:[0-5][0-9]$/
   if time1.match(time_pattern) && time2.match(time_pattern)
     sum = [""]
-  	time1 = time1.split(":")
+    time1 = time1.split(":")
     time2 = time2.split(":")
     sum[2] = time2[2] + time1[2]
     sum[1] = (sum[2].to_i.div(60)) + time2[1].to_i + time1[1].to_i
