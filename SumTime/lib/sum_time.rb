fb3b83edd257c113bcc5f5a99cb448cc177b9893
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-28 23:09:28 +0530

    s

--- SumTime/lib/sum_time.rb
+++ SumTime/lib/sum_time.rb
@@ -1,19 +1,25 @@
+#def format_time(array)
+ # array.each { |part| part = "0" + part if part.to_s.length == 1}
+#end
 def sum(time1 = "00:00:00", time2 = "00:00:00")
   time1 = "0" + time1 if time1.index(":") != 2
   time2 = "0" + time2 if time2.index(":") != 2
   time_pattern = /^([2][0-3]|[0-1][0-9]):[0-5][0-9]:[0-5][0-9]$/
   if time1.match(time_pattern) && time2.match(time_pattern)
-  	sum = []
+  	sum = [""]
   	time1 = time1.split(":")
     time2 = time2.split(":")
     sum[2] = time2[2].to_i + time1[2].to_i
-    sum[1] = (sum[2].div(60)) + time2[1].to_i + time1[1].to_i
-    sum[0] = (sum[1].div(60)) + time2[0].to_i + time1[0].to_i
-    day = sum[0].div(24)
+    sum[1] = ((sum[2].to_i.div(60)) + time2[1].to_i + time1[1].to_i)
+    sum[0] = ((sum[1].to_i.div(60)) + time2[0].to_i + time1[0].to_i)
+    day = sum[0].to_i.div(24)
     print day.to_s + " day & " if day > 0
-    sum[2] = sum[2].modulo(60)
-    sum[1] = sum[1].modulo(60)
-    sum[0] = sum[0].modulo(24)
+    sum[2] = (sum[2].to_i.modulo(60))
+    sum[1] = (sum[1].to_i.modulo(60))
+    sum[0] = (sum[0].to_i.modulo(24))
+    sum[2] = "0" + sum[2].to_s if sum[2].to_s.length == 1
+    sum[1] = "0" + sum[1].to_s if sum[1].to_s.length == 1
+    sum[0] = "0" + sum[0].to_s if sum[0].to_s.length == 1
     sum.join(":")
   else
     puts "Entered incorrect time format in either of the dates"
