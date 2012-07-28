5b5a1866dbf779ca775cc97342ce5770bc5d6558
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-28 13:50:08 +0530

    s

--- Reverse_Sentence/reverse_sentence.rb
+++ Reverse_Sentence/reverse_sentence.rb
@@ -1,4 +1,6 @@
+def reverse(input_string)
+  puts input_string.split(" ").reverse().join(" ")
+end
 puts "Enter the String"
-input_string = gets
-reverse(input_string);
-
+input_string = gets.chomp
+reverse(input_string)
