068a21b60da97ee2be89ad8a3b0262c8680b4924
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-30 01:20:54 +0530

    s

--- array_hash/bin/main.rb
+++ array_hash/bin/main.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/array_hash'
 
 a = ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ']
-a.to_hash
+a.to_hash(a.length)
--- array_hash/lib/array_hash.rb
+++ array_hash/lib/array_hash.rb
@@ -1,9 +1,19 @@
 class Array
-  def to_hash
+  def to_hash(length)
+    array2d = Array.new(length + 1){Array.new}
     hash = Hash.new(Array.new)
     for i in self
-      hash[i.to_s.size] += [i]
+      array2d[i.to_s.size] << i
     end
-    puts hash
+    array2d.inject do |mem, var|
+      if var[0].to_s.length > 0
+        if var[0].to_s.length.odd?
+          hash["odd"] += [var] 
+        else
+          hash["even"] += var
+        end
+      end
+    end
+    print hash
   end	
 end
