b970dd2adb6ee349438c68ca88bbf8878f7ae1bf
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-29 23:05:47 +0530

    s

---
+++ Raise/bin/main.rb
@@ -0,0 +1,11 @@
+require "../lib/raise.rb"
+begin
+  print "Enter the Firstname: "
+  firstname = gets.chomp
+  raise SyntaxError, "Firstname cannot be empty" if firstname == ""
+  raise SyntaxError, "Firstname is not capitalised" if firstname != firstname.capitalize
+  print "Enter the Lastname: "
+  lastname = gets.chomp
+  raise SyntaxError, "Lastname cannot be empty" if lastname == ""
+  name = Name.new(firstname, lastname)
+end
---
+++ Raise/lib/raise.rb
@@ -0,0 +1,7 @@
+class Name 
+  def initialize(firstname, lastname)
+    @firstname = firstname
+    @lastname = lastname
+    puts "Name is initialized with firstname capitalized"
+  end
+end
