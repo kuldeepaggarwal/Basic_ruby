c227968bf546fb4986c30f65cb5fc5f9f881b25d
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-30 16:28:51 +0530

    sum_19

--- SumTime_19/bin/main.rb
+++ SumTime_19/bin/main.rb
@@ -1,21 +1,21 @@
 require '../lib/SumTime_19'
-print "Enter number of dates to be added(minimum 2):"
+print "Enter number of 'time(s)' to be added(minimum 2):"
 count = (gets.chomp).to_i
-dates = Array.new()
+times = Array.new()
 if count < 2
-  puts "You have not entered minimum required number of dates"
+  puts "You have not entered minimum required number of times"
 else
   count.times do |index|
-    print "Enter #{index + 1} date([H]H:MM:SS): "
-    dates << gets.chomp
+    print "Enter #{index + 1} time([H]H:MM:SS): "
+    times << gets.chomp
   end
   no_of_days = 0
-  while dates.length > 1
-    date1 = dates.pop
-    date2 =dates.pop
-    temp_hash = sum(date1, date2)
+  while times.length > 1
+    time1 = times.pop
+    time2 =times.pop
+    temp_hash = sum(time1, time2)
     no_of_days += temp_hash["days"]
-    dates << temp_hash["date"] 
+    times << temp_hash["times"] 
   end
   puts "#{no_of_days} days & #{dates.join(":")}"
 end
