a0b156fdb5c6ef1d08f47f17212f66f601b681b2
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-30 19:47:27 +0530

    update hash

--- array_hash/lib/array_hash.rb
+++ array_hash/lib/array_hash.rb
@@ -1,17 +1,15 @@
 class Array
   def to_hash(length)
-    array2d = Array.new(length + 1){Array.new}
+    array2d = Array.new{Array.new}
     hash = Hash.new(Array.new)
     for i in self
-      array2d[i.to_s.size] << i
+      array2d[i.to_s.size] = [] if array2d[i.to_s.size].nil?
+      array2d[i.to_s.size] += [i]
     end
-    array2d.inject do |mem, var|
-      if var[0].to_s.length > 0
-        if var[0].to_s.length.odd?
-          hash["odd"] += [var] 
-        else
-          hash["even"] += var
-        end
+    array2d.inject({}) do |mem, var|
+      if !var.nil?
+        pos =  var.first.to_s.length.odd? ? "odd" : "even"
+         hash[pos] += [var]
       end
     end
     print hash
