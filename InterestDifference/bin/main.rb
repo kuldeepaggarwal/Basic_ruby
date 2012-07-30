4c1248fb2258369b6a35fc5fa4a0edc0a56db46a
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-30 13:44:22 +0530

    update

--- InterestDifference/bin/main.rb
+++ InterestDifference/bin/main.rb
@@ -1,10 +1,10 @@
 require "../lib/interest_difference.rb"
 
 print "Enter the Principal: "
-principal = gets.chomp.to_i
+principal = gets.chomp.to_f
 #print "Enter the Rate of interest in % p.a.: "
 #rate = gets.chomp.to_i
 print "Enter the time: "
-time = gets.chomp.to_i
+time = gets.chomp.to_f
 object = InterestDifference.new(principal, time)
 object.difference()
