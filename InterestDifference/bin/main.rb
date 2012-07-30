5a078f002e05a5953cf86bee6e850cd2d906ed74
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-30 13:39:38 +0530

    ID updated

--- InterestDifference/bin/main.rb
+++ InterestDifference/bin/main.rb
@@ -2,9 +2,9 @@ require "../lib/interest_difference.rb"
 
 print "Enter the Principal: "
 principal = gets.chomp.to_i
-print "Enter the Rate of interest in % p.a.: "
-rate = gets.chomp.to_i
+#print "Enter the Rate of interest in % p.a.: "
+#rate = gets.chomp.to_i
 print "Enter the time: "
 time = gets.chomp.to_i
-object = InterestDifference.new(principal, rate, time)
+object = InterestDifference.new(principal, time)
 object.difference()
--- InterestDifference/lib/interest_difference.rb
+++ InterestDifference/lib/interest_difference.rb
@@ -1,13 +1,13 @@
 require "mathn"
 class InterestDifference
-  def initialize(p , r = 10, t)
+  def initialize(p , t, r = 10)
     @p = p
     @r = r
     @t = t
   end
   def difference
    amount_simple_interest = ((@p * @t * @r) / 100) + @p
-   amount_compound_interest = (@p * ((1 + @r * 0.01) ** @t)).round
-   puts (amount_compound_interest - amount_simple_interest).abs
+   amount_compound_interest = (@p * ((1 + @r * 0.01) ** @t))
+   puts (amount_compound_interest - amount_simple_interest).round(3)
   end
 end
