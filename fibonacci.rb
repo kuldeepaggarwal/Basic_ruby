1c35c6fd43e062eb339c8e9e0dcaa85524c6aef4
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-01 13:46:00 +0530

    fib

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,16 +1,17 @@
 def fib (limit)
-  puts @sec_last = 0
-  puts @last_num = 1
   loop do
-    puts result = yield
+    result = yield @last_num + @sec_last
+    puts result
     @sec_last = @last_num
     @last_num = result
   end
 end
 
 puts "Fibonacci Series upto 1000:"
-fib(limit_upto = 1000) do 
-  break if @last_num + @sec_last > limit_upto
-  @last_num + @sec_last
+puts @sec_last = 0
+puts @last_num = 1
+fib(limit_upto = 1000) do |sum|
+  break if  sum > limit_upto
+  sum
 end
 
