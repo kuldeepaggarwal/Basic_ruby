8f0331f1ae26f16945a8ea06ef7c82c7e9200753
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-11 19:35:41 +0530

    Occurence update

--- occurences_hash.rb
+++ occurences_hash.rb
@@ -1,7 +1,8 @@
 def occurence(string)
+  string.gsub!(/[^A-z]/,"")
   hash_container = Hash.new(0)
   string.each_char do |chr| 
-    hash_container[chr] += 1 if chr!= " "
+    hash_container[chr] += 1
   end
   puts hash_container
 end
