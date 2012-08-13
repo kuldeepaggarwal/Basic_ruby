59176025d8f96dab69a52df1fe6b0d627fc6cc3c
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-14 01:44:35 +0530

    CSV

--- CSV/lib/CSV.rb
+++ CSV/lib/CSV.rb
@@ -1,23 +1,17 @@
 class CSV
   def initialize
-  	@name = ""
-  	@emp_id = ""
-    @designation = ""
-  	@designation_hash = Hash.new(Array.new())
+    @name, @emp_id, @designation, @designation_hash = "", "", "", Hash.new(Array.new()) 
   end
   def load_employees(from_file, to_file)
     File.readlines(from_file).each do |line|
-      @name, @emp_id, @designation = line.split(",")
-      @designation = @designation.gsub(/\s/, "")
-      @emp_id = @emp_id.gsub(/\s/, "")
-      @name = @name.gsub(/\s/, "")
+      @name, @emp_id, @designation = line.gsub(/\s/, "").split(",")
       @designation_hash[@designation] += [@name + "(Empid: " + @emp_id + ")"]
     end
     File.open(to_file, "w") do |file|
       @designation_hash.sort.each do |key, value|
-      	file.puts key + "s"
+        file.puts key + "s"
         file.puts value
-        file.puts 
+        file.puts
       end
     end
   end
